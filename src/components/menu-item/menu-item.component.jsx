0b49ac46660ad97271e8eed7d30dd96a73d2033b
Shohan <shohan@Shohans-iPro.cable.rcn.com>
2020-02-17 18:56:33 -0500

    +added more styling

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -4,12 +4,17 @@ import './menu-item.styles.scss'
 
 const MenuItem = ({title, imageUrl, size}) => ( //destructured from props, same as props.title but we're pulling title value and setting it inside
     <div 
-    className={`${size} menu-item` }
-    style={{backgroundImage: `url(${imageUrl})`}
-    }>
+    className={`${size} menu-item` }>
+    <div 
+    className="background-image"  
+    style={{backgroundImage: `url(${imageUrl})`
+    }}
+    />
+
+   
     <div className="content">
-        <h1 className="title">{title}</h1>
-        <span className="subtitle">Shop Now</span>
+        <h1 className="title">{title.toUpperCase()}</h1>
+        <span className="subtitle">{("Shop Now").toUpperCase()}</span>
     </div>
 </div>
 )
