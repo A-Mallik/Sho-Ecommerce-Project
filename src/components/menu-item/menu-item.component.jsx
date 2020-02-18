935a0a84d15d46c5f17487d8bcc5939ac68e65a1
Shohan <shohan@Shohans-iPro.cable.rcn.com>
2020-02-17 23:52:28 -0500

    fixed back button issue

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -5,9 +5,14 @@ import './menu-item.styles.scss'
 
 const MenuItem = ({title, imageUrl, size, history, linkUrl, match}) => ( //destructured from props, same as props.title but we're pulling title value and setting it inside
     <div 
-    className={`${size} menu-item` } onClick={ () => history.replace(`${match.url}${linkUrl}`)}>
+    className={`${size} menu-item` } onClick={ () => history.push(`${match.url}${linkUrl}`)}>
+
+{/* <div
+    className={`${size} menu-item`}
+    onClick={() => history.push(`${match.url}${linkUrl}`)}
+  > */}
     {/* `${console.log('Match URL:' + JSON.stringify(match))}` */}
-    {/* history.push is a method, history.replace is another method that can also be used except it doesnt "push" an entry to history. */}
+    {/* history.push is a method, history.replace is another method that can also be used except it doesnt "push" an entry to history. Breaks back button */}
     <div 
     className="background-image"  
     style={{backgroundImage: `url(${imageUrl})`
